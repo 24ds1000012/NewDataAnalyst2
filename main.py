bbdc8f9f81ae61b7bea8c185239174e001679583
Diwkar Yasarapu <diwakar0512@users.noreply.huggingface.co>
2025-08-03 14:24:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def analyze(file: UploadFile = File(...)):
         )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=7860)
 
 
 """
