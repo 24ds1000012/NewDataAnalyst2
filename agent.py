c9bf278dc98c9405822de5ee1cc495429af90cfe
Diwkar Yasarapu <diwakar0512@users.noreply.huggingface.co>
2025-08-05 07:41:35 +0000

    Update agent.py

--- agent.py
+++ agent.py
@@ -19,6 +19,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 from sklearn.linear_model import LinearRegression
 import duckdb
 
